d7ed98a10687616f04381564fa9c241db9ed8ce8
Moringa School <abdallah.abdul@student.moringaschool.com>
2021-06-04 08:27:27 +0300

    add initial changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+
 public class App {
+    public static void main( String[] args ) {
+        map<String, Object> model = new HashMap<~>();
+        ProcessBuilder process = new ProcessBuilder( );
+        Integer port;
+        If (process.environment().get("PORT"))
+    }
+
 }
