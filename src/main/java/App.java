e3d7556a6ceabd50f28f46b9506ba3b13eb23fe1
Moringa School <abdallah.abdul@student.moringaschool.com>
2021-06-05 22:56:03 +0300

    add the new file and make some changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,12 @@ public class App {
         map<String, Object> model = new HashMap<~>();
         ProcessBuilder process = new ProcessBuilder( );
         Integer port;
-        If (process.environment().get("PORT"))
+        If (process.environment().get("PORT") !=null){
+            port = Integer.parseInt( process.environment().get( "PORT" ) );
+        }else {
+            port = 4567;
+        }
+        setPort(port);
     }
 
 }
